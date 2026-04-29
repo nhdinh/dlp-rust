3fa00645c2f3b73330c6c584d20529b69dbb86f0
Hung Dinh <hung.dnc@gmail.com>
2026-04-29 13:08:49 +0700

    chore(31-01): link cfgmgr32.lib and advapi32.lib in build.rs
    - Add cargo:rustc-link-lib=cfgmgr32 for CM_* APIs
    - Add cargo:rustc-link-lib=advapi32 for ACL manipulation APIs
    - Both gated behind #[cfg(windows)]

--- dlp-agent/build.rs
+++ dlp-agent/build.rs
@@ -2,4 +2,10 @@ fn main() {
     println!("cargo:rerun-if-changed=proto/content_analysis.proto");
     prost_build::compile_protos(&["proto/content_analysis.proto"], &["proto/"])
         .expect("protobuf compilation failed");
+
+    #[cfg(windows)]
+    {
+        println!("cargo:rustc-link-lib=cfgmgr32");
+        println!("cargo:rustc-link-lib=advapi32");
+    }
 }
